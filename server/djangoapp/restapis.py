a476d8242c920a99c719cbdce991fbc735ba74c1
Franck Blaevoet <franck_blaevoet@yahoo.fr>
2021-10-15 13:21:52 +0000

    some progress on NLU

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -10,11 +10,28 @@ from requests.auth import HTTPBasicAuth
 #                                     auth=HTTPBasicAuth('apikey', api_key))
 def get_request(url, **kwargs):
     print(kwargs)
+    print(kwargs["dealerID"])
     print("GET from {} ".format(url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(url, headers={'Content-Type': 'application/json'},
                                     params=kwargs)
+    except:
+        # If any error occurs
+        print("Network exception occurred")
+    status_code = response.status_code
+    print("With status {} ".format(status_code))
+    json_data = json.loads(response.text)
+    return json_data
+
+def get_request_with_key(url, api_key, **kwargs):
+    print(kwargs)
+    print(kwargs["dealerID"])
+    print("GET from {} ".format(url))
+    try:
+        # Call get method of requests library with URL and parameters
+        response = requests.get(url, headers={'Content-Type': 'application/json'},
+                                    params=kwargs, auth=HTTPBasicAuth('apikey', api_key) )
     except:
         # If any error occurs
         print("Network exception occurred")
@@ -123,7 +140,11 @@ def get_dealer_reviews_from_cf (url, dealerId):
             review_obj = DealerReview(dealership=review_doc["dealership"], name=review_doc["name"], purchase=review_doc["purchase"],
                                    review=review_doc["review"], purchase_date=review_doc["purchase_date"], car_make=review_doc["car_make"],
                                    car_model=review_doc["car_model"],
-                                   car_year=review_doc["car_year"], id=review_doc["id"], sentiment="GOOD")
+                                   car_year=review_doc["car_year"], id=review_doc["id"], sentiment="")
+
+            # Getting the sentiment
+            review_obj.sentiment = analyze_review_sentiments(review_obj.review)
+
             results.append(review_obj)
 
     return results
@@ -134,6 +155,6 @@ def get_dealer_reviews_from_cf (url, dealerId):
 # def analyze_review_sentiments(text):
 # - Call get_request() with specified arguments
 # - Get the returned sentiment label such as Positive or Negative
-
+def analyze_review_sentiments(dealerreview)
 
 
